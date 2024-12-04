f9c4f461845138f3603218396436a11eb313cbc2
Will-Helliwell <wj.helliwell@gmail.com>
2024-12-04 11:55:49 +0800

    Update comment.

--- page_templates/page_homepage.php
+++ page_templates/page_homepage.php
@@ -9,14 +9,12 @@
 <!-- Get astra header -->
 <?php get_header(); ?>
 
-<!-- Replicate astra default template outer html for styling and SEO -->
+<!-- Replicate astra default page template outer html for styling and SEO -->
 <div id="primary" class="content-area primary">
     <main id="main" class="site-main">
         <article id="post-<?php the_ID(); ?>" <?php post_class('ast-article-single'); ?> itemscope="itemscope" itemtype="https://schema.org/CreativeWork">
-
             <?php get_template_part('template_parts/astra_toggleable_title'); ?>
 
-            
             <?php get_template_part('template_parts/astra_gutenberg_blocks') ?>
         </article>
     </main>
